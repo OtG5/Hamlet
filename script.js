617a5f14def1744bb43fc9fb63dfd672aff19744
Oscar <liloscar101@gmail.com>
2023-08-21 07:43:36 -0400

    changed hi to h1 line18

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ for (let i = 0; i < phrases.length; i++) {
            let phrase = document.createElement("h1");
            phrase.textContent = this.textContent; 
 
-           let footnote = document.createElement("hi");
+           let footnote = document.createElement("h1");
            footnote.textContent = footnotes[i];
            footnote.style = "font-style: italc; font-size: 1.2em;";
 
